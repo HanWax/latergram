91707c05523277263773a52e851901bb3a1839c9
Hannah Waxman <hannah.waxman8@gmail.com>
2014-08-19 16:26:55 +0100

    Added filtering by tag functionality

--- spec/features/tagging_posts_feature_spec.rb
+++ spec/features/tagging_posts_feature_spec.rb
@@ -11,8 +11,27 @@ describe 'tagging posts' do
 		it 'displays tags as links under posts' do 
 			visit('/')
 			fill_in 'post_tag_list', with: '#yolo, #swag'
-			expect(page).to have_link('#swag')
+			click_button 'Add post'
 			expect(page).to have_link('#yolo')
+			expect(page).to have_link('#swag')
 		end  
+	end 
+
+end 
+
+describe 'filtering by tags' do 
+	before do 
+		Post.create(title: 'Amazon Bob', tag_list: '#amazon, #bob')
+		Post.create(title: 'Amazing Bob', tag_list: '#bob')
+	end 
+
+	context 'displaying tagged posts' do 
+		it 'filters to show only tagged posts' do 
+			visit '/'
+			click_link '#amazon'
+			expect(page).to have_css 'h1', text: 'Posts tagged with:'
+			expect(page).to have_content 'Amazon Bob'
+			expect(page).not_to have_content 'Amazing Bob'
+		end 
 	end 
 end 
