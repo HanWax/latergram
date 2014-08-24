e413cad58ea6ed5a7ea96cafd81f1dfe074d250f
Hannah Waxman <hannah.waxman8@gmail.com>
2014-08-24 21:49:42 +0100

    Updated styling, ready for heroku deployment

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-	$('.like-link').on('click', function(event){
-	event.preventDefault():
-	$.post(this.href, function(response){
-		alert(response.new_like_count);
-	});
+	$('.like-link').on('click', function(event) {
+		console.log('hi')
 	})
 })
