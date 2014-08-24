ed1a3f72542ed8f431e1a246f0bc78ed21ebbaf6
Hannah Waxman <hannah.waxman8@gmail.com>
2014-08-24 16:20:26 +0100

    Updated styling of website

---
+++ vendor/assets/javascripts/Latergram_files/maps.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+$.get('/posts/'+ $('#map').data("id"), function(postInfo) { 
+
+	var map = new GMaps({
+	  div: '#map',
+	  lat: -12.043333,
+	  lng: -77.028333
+	});
+
+	GMaps.geocode({
+	  address: postInfo.address,
+	  callback: function(results, status) {
+	    if (status == 'OK') {
+	      var latlng = results[0].geometry.location;
+	      map.setCenter(latlng.lat(), latlng.lng());
+	      map.addMarker({
+	        lat: latlng.lat(),
+	        lng: latlng.lng()
+	      });
+	    }
+	  }
+	});
+   });
+});
+
